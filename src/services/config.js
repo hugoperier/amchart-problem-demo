43f70d3233724f7e3ea826de700347dbf8b08747
Hugo Win64 <hugo.perier@epitech.eu>
2020-02-25 15:27:09 +0100

    clean

--- src/services/config.js
+++
@@ -1,8 +0,0 @@
-import config from '../config.json'
-
-export const withConfiguration = (cb) =>  {
-    const env = process.env.REACT_APP_ENV?.replace(/\s/g,'')
-    if (env) {
-        return cb(config[env])
-    }
-}
